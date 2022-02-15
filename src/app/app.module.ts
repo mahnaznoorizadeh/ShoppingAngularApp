f73c253cf6f340d6f9780f3f0e5070fff43dbb82
Mahnaz Noori Zadeh <mza@f5it.com>
2022-02-15 12:40:21 +0100

    tt

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AppRotingModule } from './app-routing.module';
+import { CoreModule } from './core.module';
+import { shoppingListReducer } from './shopping-list/store/shopping-list-reducer';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+  ],
+  imports: [  
+    BrowserModule, 
+    FormsModule,
+    HttpClientModule,
+    AppRotingModule,
+    StoreModule.forRoot({shoppingList: shoppingListReducer}),
+    CoreModule, 
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -0,0 +1,35 @@
+import { Action, ActionCreatorProps } from "@ngrx/store";
+import { Ingredient } from "../../shared/ingredient.model";
+
+export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
+
+//important: "payload" is Not a name you have to use!
+// The Action interface only forces you to add a "type" property!
+export class AddIngredient implements Action {
+  readonly type = ADD_INGREDIENT;
+  
+  constructor(public payload: Ingredient) {}
+}
+
+export class AddIngredients implements Action {
+  readonly type = ADD_INGREDIENTS;
+
+  constructor(public payload: Ingredient[]) {}
+}
+
+export class UpdateIngredients implements Action {
+  readonly type = UPDATE_INGREDIENT;
+
+  constructor(public payload: {index: number, ingredient: Ingredient}) {}
+}
+
+export class DeleteIngredient implements Action {
+  readonly type = DELETE_INGREDIENT; 
+  
+  constructor(public payload: number) {}
+}
+
+export type ShoppingListActions = AddIngredient | AddIngredients | UpdateIngredients | DeleteIngredient;
